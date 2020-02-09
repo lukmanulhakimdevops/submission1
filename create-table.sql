01646e9ecc4f5bea0409f5a28c1a95a4e5cf7c85
null32sys <coldcloud87@gmail.com>
2020-02-09 15:02:37 +0700

    Update create-table.sql

--- create-table.sql
+++ create-table.sql
@@ -1,4 +1,4 @@
-create table [dbo].[lukmanulhakimdicodingbookcatalougesubmission](
+create table [dbo].[dicodingdb](
     id INT NOT NULL IDENTITY(1,1) PRIMARY KEY(id),
     name VARCHAR(30),
     address VARCHAR(30),
