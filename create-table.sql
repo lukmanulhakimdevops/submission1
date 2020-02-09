56445bc8194a34ecf7103891076fcfba2770b15f
null32sys <coldcloud87@gmail.com>
2020-02-09 13:30:03 +0700

    Update create-table.sql

--- create-table.sql
+++ create-table.sql
@@ -1,7 +1,7 @@
 create table [dbo].[dbazure](
     id INT NOT NULL IDENTITY(1,1) PRIMARY KEY(id),
     name VARCHAR(30),
-	address VARCHAR(30),
+    address VARCHAR(30),
     email VARCHAR(30),
     job VARCHAR(30),
     date DATE
