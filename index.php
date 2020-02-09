895578a297e58cd49f9c638f04b340d6d955dd04
null32sys <coldcloud87@gmail.com>
2020-02-09 14:43:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
     $host = "lukmanulhakimdicodingbookcatalougesubmission.database.windows.net";
     $user = "lukmanulhakim";
     $pass = "$Abcd12345";
-    $db = "lukmanulhakim";
+    $db = "lukmanulhakimdicodingbookcatalougesubmission";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
@@ -47,7 +47,7 @@
             $job = $_POST['job'];
             $date = date("d-m-Y");
             // Insert data
-            $sql_insert = "INSERT INTO lukmanulhakim (name, address,email, job, date) 
+            $sql_insert = "INSERT INTO lukmanulhakimdicodingbookcatalougesubmission (name, address,email, job, date) 
             VALUES (?,?,?,?,?)";
             $stmt = $conn->prepare($sql_insert);
             $stmt->bindValue(1, $name);
@@ -63,14 +63,14 @@
         echo "<h3>Your're registered!</h3>";
     } else if (isset($_POST['load_data'])) {
         try {
-            $sql_select = "SELECT * FROM lukmanulhakim";
+            $sql_select = "SELECT * FROM lukmanulhakimdicodingbookcatalougesubmission";
             $stmt = $conn->query($sql_select);
             $registrants = $stmt->fetchAll(); 
             if(count($registrants) > 0) {
                 echo "<h2>People who are registered:</h2>";
                 echo "<table>";
                 echo "<tr><th>Name</th>";
-				echo "<th>Address</th>";
+		echo "<th>Address</th>";
                 echo "<th>Email</th>";
                 echo "<th>Job</th>";
                 echo "<th>Date</th></tr>";
